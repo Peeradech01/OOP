217b1668c1e0192a7d56f0d4d109ecfb3778279e
Peeradech01 <109953663+Peeradech01@users.noreply.github.com>
2023-01-26 10:41:53 +0700

    Add files via upload

---
+++ Lab01/src/CircleArea.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class  CircleArea{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println(area);
+    }
+    
+}
