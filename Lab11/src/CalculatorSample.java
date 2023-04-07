0e3558408556ccf2c0b8ac616f459001505423f2
Peeradech01 <109953663+Peeradech01@users.noreply.github.com>
2023-04-07 10:20:22 +0700

    Update CalculatorSample.java

--- Lab11/src/CalculatorSample.java
+++ Lab11/src/CalculatorSample.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-
+import java.util.ArrayList;
 public class CalculatorSample implements ActionListener{
     private JFrame fr;
     private JButton bn1, bn2, bn3, bn4, bn5, bn6, bn7, bn8, bn9, bn10, bn11, bn12, bn13, bn14, bn15, bn16;
@@ -10,11 +10,13 @@ public class CalculatorSample implements ActionListener{
     
     private double num1;
     private double num2;
-    private String op;
+    private String operator;
+    private double result;
     
     public CalculatorSample(){
         fr = new JFrame("MY Calculator");
         tf = new JTextField();
+        tf.setHorizontalAlignment(JTextField.RIGHT);
         pn = new JPanel();
         bn1 = new JButton("7");
         bn2= new JButton("8");
@@ -58,12 +60,90 @@ public class CalculatorSample implements ActionListener{
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fr.setSize(300, 350);
         fr.setVisible(true);
+        
+        bn1.addActionListener(this);
+        bn2.addActionListener(this);
+        bn3.addActionListener(this);
+        bn4.addActionListener(this);
+        bn5.addActionListener(this);
+        bn6.addActionListener(this);
+        bn7.addActionListener(this);
+        bn8.addActionListener(this);
+        bn9.addActionListener(this);
+        bn10.addActionListener(this);
+        bn11.addActionListener(this);
+        bn12.addActionListener(this);
+        bn13.addActionListener(this);
+        bn14.addActionListener(this);
+        bn15.addActionListener(this);
+        bn16.addActionListener(this);
+        
+        
+        
+        
     }
     
     @Override
     public void actionPerformed(ActionEvent e){
         
-    }
+        ArrayList op = new ArrayList();
+        op.add("+");
+        op.add("-");
+        op.add("x");
+        op.add("/");
+        op.add("=");
+        op.add("c");
+        
+        if(!op.contains(e.getActionCommand())){
+            tf.setText(tf.getText()+e.getActionCommand());
+        
+        }
+        else{
+            if(e.getSource().equals(bn4)){
+                num1 = Integer.parseInt(tf.getText());
+                operator = "+";
+                tf.setText("");
+            }
+            else if (e.getSource().equals(bn8)){
+                num1 = Integer.parseInt(tf.getText());
+                operator = "-";
+                tf.setText("");
+            }
+            else if (e.getSource().equals(bn12)){
+                num1 = Integer.parseInt(tf.getText());
+                operator = "x";
+                tf.setText("");
+            }
+            else if (e.getSource().equals(bn16)){
+                num1 = Integer.parseInt(tf.getText());
+                operator = "/";
+                tf.setText("");
+            }
+            else if (e.getSource().equals(bn14)){
+                tf.setText("");
+            }
+            else if (e.getSource().equals(bn15)){ // =
+                num2 = Integer.parseInt(tf.getText());
+                if(operator.equals("+")){
+                    result = num1+num2;
+                }
+                else if (operator.equals("-")){
+                    result = num1-num2;
+                }
+                else if (operator.equals("x")){
+                    result = num1*num2;
+                }
+                else if (operator.equals("/")){
+                    result = num1/num2;
+                }
+                tf.setText(result+"");
+                
+                
+            }
+            
+        
+        
+        }
+    }   
     
 }
-
