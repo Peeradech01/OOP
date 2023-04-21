6c8dfb2d30d6a6aa9e5e03bd5c158a0c922becd9
Peeradech01 <109953663+Peeradech01@users.noreply.github.com>
2023-04-21 11:44:01 +0700

    Update ChatDemo.java

--- Lab12/src/ChatDemo.java
+++ Lab12/src/ChatDemo.java
@@ -1,13 +1,19 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class ChatDemo {
+
+public class ChatDemo implements ActionListener, WindowListener{
     private JFrame fr;
     private JTextField tf;
     private JTextArea ta;
     private JButton Submit, Reset;
     private JPanel pn;
+    private File file;
+    
     
     public ChatDemo(){
         fr = new JFrame("ChatDemo");
@@ -29,11 +35,93 @@ public class ChatDemo {
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fr.pack();
         fr.setVisible(true);
+        
+        file = new File("ChatDemo.dat");
+        try {
+            file.createNewFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     
-    
+        Submit.addActionListener(this);
+        Reset.addActionListener(this);
+        fr.addWindowListener(this);
+        
+        
     }
+    
+    
+    public void actionPerformed(ActionEvent ev){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.print(dtf.format(LocalDateTime.now()));
+        if (ev.getSource().equals(Submit)){
+            ta.setText(ta.getText()+ "\n" +dtf.format(LocalDateTime.now())+" : "+ tf.getText());
+            tf.setText("");
+        }
+        else if (ev.getSource().equals(Reset)){
+            ta.setText("");
+        
+        }   
+    }
+    
+    public void windowOpened(WindowEvent e) {
+            if (file.exists()) {
+            String data = "";
+            try (FileReader fr = new FileReader(file)) {
+                int chr;
+                while ((chr = fr.read()) != -1) {
+                    data += (char) chr;
+                }
+                System.out.println(data);
+                ta.setText(data);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+                System.out.println("File not found.");
+            }
+    }
+    
+    
+    public void windowClosing(WindowEvent e) {
+        try (FileWriter fw = new FileWriter(file)) {
+            String data = ta.getText();
+            for (int i = 0; i < data.length(); i++) {
+                fw.write(data.charAt(i));
+                }
+            System.out.println("Save data");
+        } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    
     
     public static void main(String arge[]){
         new ChatDemo();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
